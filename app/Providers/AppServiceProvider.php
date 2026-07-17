765b45738e1ba0d287dc394dce443b9a3527e9cc
AnggaSaputra78 <babarian433@gmail.com>
2026-07-17 14:54:15 +0700

    fix: force https for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Tambahan Facade URL
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Paksa semua asset & link menggunakan HTTPS jika tidak di komputer lokal
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
